d9af3a21e2a4c46f744b8cf05ac49a2a47073438
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-03-26 17:33:16 -0500

    training

--- JavaFoundations/src/javafoundations/JavaFoundations.java
+++ JavaFoundations/src/javafoundations/JavaFoundations.java
@@ -15,7 +15,39 @@ public class JavaFoundations {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello, World!");
+        // Variables
+        /* Data Types */
+        // Primitives
+        byte myByte = 127;
+        System.out.println("myByte: " + myByte);
+        short myShort = 32767;
+        System.out.println("myShort: " + myShort);
+        int myInteger = 2147483647;
+        System.out.println("myInteger: " + myInteger);
+        long myLong = 922337203;
+        System.out.println("myLong: " + myLong);
+        float myFloat = 0.9999999f;
+        System.out.println("myFloat: " + myFloat);
+        double myDouble = 0.999999999999999;
+        System.out.println("myDouble: " + myDouble);
+        boolean myBoolean = true;
+        System.out.println("myBoolean: " + myBoolean);
+        char myCharacter = 'a';
+        System.out.println("myCharacter: " + myCharacter);
+        /*
+        About the error: "incompatible types: String cannot be converted to char"
+        The "string" we declare them using double quotes,
+        the 'char' we declare them using single quotes.
+        Example:
+        String myString = "javi";
+        char myCharacter = 'j';
+        */
+        // Non-primitives
+        String myString = "Hello, World!";
+        System.out.println("myString: " + myString);
+        /* OPERATORS */
+        /* CONTROL STRUCTURES */
+        
     }
     
 }
