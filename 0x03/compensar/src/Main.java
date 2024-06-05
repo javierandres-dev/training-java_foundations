db1de3c9d2caddcf3cb34790e9c329cc7535d091
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-06-05 12:44:46 -0500

    training

---
+++ 0x03/compensar/src/Main.java
@@ -0,0 +1,56 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+        */
+
+        //System.out.println("works!");
+
+        /*
+        final int primerNumero = 650, segundoNumero = 120, tercerNumero = 330;
+        String conversionPrimerNumero = "Primer número: " + primerNumero +
+                "\n\t en binario: " + Integer.toBinaryString(primerNumero) +
+                "\n\t en octal: " + Integer.toOctalString(primerNumero) +
+                "\n\t en hexadecimal: " + Integer.toHexString(primerNumero);
+        System.out.println(conversionPrimerNumero);
+        String conversionSegundoNumero = "Segundo número: " + segundoNumero +
+                "\n\t en binario: " + Integer.toBinaryString(segundoNumero) +
+                "\n\t en octal: " + Integer.toOctalString(segundoNumero) +
+                "\n\t en hexadecimal: " + Integer.toHexString(segundoNumero);
+        System.out.println(conversionSegundoNumero);
+        String conversionTercerNumero = "Tercer número: " + tercerNumero +
+                "\n\t en binario: " + Integer.toBinaryString(tercerNumero) +
+                "\n\t en octal: " + Integer.toOctalString(tercerNumero) +
+                "\n\t en hexadecimal: " + Integer.toHexString(tercerNumero);
+        System.out.println(conversionTercerNumero);
+        */
+
+        /*
+        final int primerNumero = 650, segundoNumero = 120, tercerNumero = 330;
+        final String primerNumeroBinario = Integer.toBinaryString(primerNumero),
+                segundoNumeroBinario = Integer.toBinaryString(segundoNumero),
+                tercerNumeroBinario = Integer.toBinaryString(tercerNumero);
+        String conversionPrimerNumeroBinario = "Primer número en binario: " + primerNumeroBinario +
+                "\n\t en decimal: " + Integer.parseInt(primerNumeroBinario, 2);
+        System.out.println(conversionPrimerNumeroBinario);
+        String conversionSegundoNumeroBinario = "Segundo número binario: " + segundoNumeroBinario +
+                "\n\t en decimal: " + Integer.parseInt(segundoNumeroBinario, 2);
+        System.out.println(conversionSegundoNumeroBinario);
+        String conversionTercerNumeroBinario = "Tercer número en binario: " + tercerNumeroBinario +
+                "\n\t en decimal: " + Integer.parseInt(tercerNumeroBinario, 2);
+        System.out.println(conversionTercerNumeroBinario);
+        */
+
+        //System.out.print("\nCurso \tnivel básico de java, \ninicia el 22 de abril del 2024 \ny finaliza el 26 de abril del \t2024\n");
+    }
+}
