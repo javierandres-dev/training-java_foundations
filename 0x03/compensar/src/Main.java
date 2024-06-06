31822e85c9eb3df4b7432a1c1dd9cac08da5ea71
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-06-06 11:09:44 -0500

    training

--- 0x03/compensar/src/Main.java
+++ 0x03/compensar/src/Main.java
@@ -1,22 +1,10 @@
+import java.util.Scanner;
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        /*
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-        */
-
-        //System.out.println("works!");
-
-        /*
+    private static void exercise1() {
         final int primerNumero = 650, segundoNumero = 120, tercerNumero = 330;
         String conversionPrimerNumero = "Primer número: " + primerNumero +
                 "\n\t en binario: " + Integer.toBinaryString(primerNumero) +
@@ -33,9 +21,9 @@ public class Main {
                 "\n\t en octal: " + Integer.toOctalString(tercerNumero) +
                 "\n\t en hexadecimal: " + Integer.toHexString(tercerNumero);
         System.out.println(conversionTercerNumero);
-        */
+    }
 
-        /*
+    private static void exercise2() {
         final int primerNumero = 650, segundoNumero = 120, tercerNumero = 330;
         final String primerNumeroBinario = Integer.toBinaryString(primerNumero),
                 segundoNumeroBinario = Integer.toBinaryString(segundoNumero),
@@ -49,8 +37,85 @@ public class Main {
         String conversionTercerNumeroBinario = "Tercer número en binario: " + tercerNumeroBinario +
                 "\n\t en decimal: " + Integer.parseInt(tercerNumeroBinario, 2);
         System.out.println(conversionTercerNumeroBinario);
-        */
+    }
 
-        //System.out.print("\nCurso \tnivel básico de java, \ninicia el 22 de abril del 2024 \ny finaliza el 26 de abril del \t2024\n");
+    private static void exercise3() {
+        System.out.print("\nCurso \tnivel básicoo\b de java, \ninicia el 22 de abrr\bil del 2024 \ny finalizaa\b el 26 de abril del \t2024\n");
+    }
+
+    private static void exercise4() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a name: ");
+        final String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+
+    private static void exercise5() {
+        final String name = JOptionPane.showInputDialog("Enter a name");
+        JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
+    }
+
+    private static void exercise6() {
+        final String shortcut = "soutv";
+        System.out.println("shortcut = " + shortcut);// shortcut: soutv
+    }
+
+    private static void exercise7() {
+        final String fileName = "Imagen_de_un_perro.jpg";
+        final int dotIndex = fileName.indexOf(".");
+        final String extension = fileName.substring(dotIndex + 1);
+        System.out.println("Extensión del archivo = " + extension.toUpperCase());
+    }
+
+    private static void exercise8() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("**** FACTURA ****");
+        System.out.print("Nombre del primer producto: ");
+        final String np1 = sc.nextLine();
+        System.out.print("Precio del primer producto: ");
+        final Double pp1 = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Nombre del segundo producto: ");
+        final String np2 = sc.nextLine();
+        System.out.print("Precio del segundo producto: ");
+        final Double pp2 = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Nombre del comprador: ");
+        final String nc = sc.nextLine();
+        final String subtotal = "\tSUBTOTAL" +
+                "\n\t\tProducto1: " + np1 + ", $" + pp1 +
+                "\n\t\tProducto2: " + np2 + ", $" + pp2 +
+                "\n\t\tSubtotal (antes de impuestos): " + (pp1 + pp2);
+        System.out.println(subtotal);
+        final double impuestos = ((pp1 + pp2) * 0.19);
+        final String total = "\tTOTAL" +
+                "\n\t\tImpuestos (19%): $" + impuestos +
+                "\n\t\tTotal (con impuestos): " + ((pp1 + pp2) + impuestos);
+        System.out.println(total);
+        System.out.println(nc + ", Gracias por tu compra!");
+    }
+
+    public static void main(String[] args) {
+        /*
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }*/
+
+        //System.out.println("works!");
+
+        //exercise1();
+        //exercise2();
+        //exercise3();
+        //exercise4();
+        //exercise5();
+        //exercise6();
+        exercise7();
+        //exercise8();
     }
 }
